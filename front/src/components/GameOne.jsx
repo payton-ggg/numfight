f0c7e8c4fb398e220a69f743959bf78f88d2c2ed
Platon <platon20081019@gmail.com>
2024-10-28 17:35:06 +0200

    asdf

--- front/src/components/GameOne.jsx
+++ front/src/components/GameOne.jsx
@@ -1,38 +1,75 @@
 import { useEffect, useState } from "react";
 
 const GameOne = () => {
-  const [task, setTask] = useState(0);
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [operation, setOperation] = useState("+");
+  const [userAnswer, setUserAnswer] = useState("");
+  const [score, setScore] = useState(0);
+  const [message, setMessage] = useState("");
 
   const generateRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  // Функция для выбора случайного арифметического оператора
   const generateRandomOperation = () => {
     const operations = ["+", "-", "*"];
-    // Выбираем случайный оператор из массива
     const randomIndex = Math.floor(Math.random() * operations.length);
     return operations[randomIndex];
   };
-  useEffect(() => {
-    const num1 = generateRandomNumber(1, 100);
-    const num2 = generateRandomNumber(1, 100);
+
+  const generateExample = () => {
     const operation = generateRandomOperation();
+    const num1 = generateRandomNumber(1, operation === "*" ? 10 : 100);
+    const num2 = generateRandomNumber(1, operation === "*" ? 10 : 100);
 
-    let startTask = `${num1} ${operation} ${num2}`;
+    setNum1(num1);
+    setNum2(num2);
+    setOperation(operation);
+    setUserAnswer("");
+    setMessage("");
+  };
 
-    setTask(startTask);
+  const checkAnswer = () => {
+    let correctAnswer = eval(`${num1} ${operation} ${num2}`);
+    console.log(correctAnswer);
 
-    console.log(`${num1} ${operation} ${num2}`, eval(startTask));
-  }, [setTask]);
+    if (parseInt(userAnswer) === correctAnswer) {
+      setScore(score + 1);
+      setMessage("Правильно!");
+    } else {
+      setMessage(`Неправильно! Правильный ответ: ${correctAnswer}`);
+    }
+
+    // Генерация нового примера после проверки
+    generateExample();
+  };
+
+  useEffect(() => {
+    generateExample();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <>
-      <div className="text-center text-8xl">{task}</div>
-      <div className="flex">
-        <div>Score</div>
+    <div className="flex justify-center items-center flex-col">
+      <div className="text-center text-5xl">Score {score}</div>
+      <div className="text-center text-8xl">
+        {num1} {operation} {num2}
       </div>
-    </>
+      <div>
+        <input
+          type="number"
+          value={userAnswer}
+          onChange={(e) => setUserAnswer(e.target.value)}
+        />
+      </div>
+      <button
+        className="bg-green-400 hover:bg-green-600 duration-[400ms] text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+        onClick={checkAnswer}
+      >
+        Download
+      </button>
+    </div>
   );
 };
 
--- front/src/components/StartScreen.jsx
+++ front/src/components/StartScreen.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const StartScreen = ({ setShow }) => {
   return (
-    <div className="bg-[#faebd9] flex flex-col items-center mt-[10%] mx-[20%] rounded-xl shadow-xl h-[50vh]">
+    <div className="bg-[#faebd9] flex flex-col items-center mt-[7%] mx-[20%] rounded-xl shadow-xl h-[70vh]">
       <div className="flex flex-row w-full justify-between">
         <div className="flex flex-row gap-1 mt-2 ml-2">
           <div className="h-4 w-4 border-fancy-1 bg-[#fea699] border-2 border-black" />
