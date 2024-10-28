dfc23c915b6c9611a8f185d8e320aaf9208c5e4b
Platon <platon20081019@gmail.com>
2024-10-28 17:36:42 +0200

    фвіафі

--- front/src/components/GameOne.jsx
+++ front/src/components/GameOne.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 
 const GameOne = () => {
@@ -47,7 +48,6 @@ const GameOne = () => {
 
   useEffect(() => {
     generateExample();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
