69f9f1ec4f219e2d917fbb12176c505f81cb4b25
Platon <platon20081019@gmail.com>
2024-11-10 01:11:44 +0200

    finally

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 const Layout = ({ children, setShow }) => {
   return (
-    <div className="bg-[#faebd9] flex flex-col items-center mt-[7%] mx-[20%] max-md:mx-[8%] rounded-xl shadow-xl h-[70vh]">
+    <div className="bg-[#faebd9] flex flex-col items-center mt-[5%] mx-[20%] max-md:mx-[8%] max-md:mt-[70px] rounded-xl shadow-xl h-[80vh]">
       <div className="flex flex-row w-full justify-between">
         <div className="flex flex-row gap-1 mt-2 ml-2">
           <div className="h-4 w-4 border-fancy-1 bg-[#fea699] border-2 border-black" />
