2a107131d7cbe8d6116395c21e0d7982315d2592
Platon <platon20081019@gmail.com>
2024-11-09 23:24:08 +0200

    do it clear

--- src/components/Marathon/Marathon.jsx
+++ src/components/Marathon/Marathon.jsx
@@ -1,27 +1,93 @@
-import GameOne from "./MarathonEnd";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import Layout from "../../Layout";
 
 // eslint-disable-next-line react/prop-types
 const Marathon = ({ setShow }) => {
-  return (
-    <div className="bg-[#faebd9] flex flex-col items-center mt-[7%] mx-[20%] max-md:mx-[8%] rounded-xl shadow-xl h-[70vh]">
-      <div className="flex flex-row w-full justify-between">
-        <div className="flex flex-row gap-1 mt-2 ml-2">
-          <div className="h-4 w-4 border-fancy-1 bg-[#fea699] border-2 border-black" />
-          <div className="h-4 w-4 border-fancy-2 bg-[#ffae9d] border-2 border-black" />
-          <div className="h-4 w-4 border-fancy-3 bg-[#807b82] border-2 border-black" />
-        </div>
-      </div>
-      <div className="">
-        <div
-          className="text-6xl text-center text-slate-700 hover:cursor-pointer"
-          onClick={() => setShow(1)}
-        >
-          NUMFIGHT
-        </div>
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [operation, setOperation] = useState("+");
+  const [userAnswer, setUserAnswer] = useState("");
+  const [score, setScore] = useState(0);
+  const [attempts, setAttempts] = useState(0);
 
-        <GameOne setShow={setShow} />
+  const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const generateRandomOperation = () => {
+    const operations = ["+", "-", "*"];
+    const randomIndex = Math.floor(Math.random() * operations.length);
+    return operations[randomIndex];
+  };
+
+  const generateExample = () => {
+    const operation = generateRandomOperation();
+    const num1 = generateRandomNumber(1, operation === "*" ? 10 : 100);
+    const num2 = generateRandomNumber(1, operation === "*" ? 10 : 100);
+
+    setNum1(num1);
+    setNum2(num2);
+    setOperation(operation);
+    setUserAnswer("");
+  };
+
+  const checkAnswer = () => {
+    let correctAnswer = eval(`${num1} ${operation} ${num2}`);
+    console.log(correctAnswer);
+    if (parseInt(userAnswer) === correctAnswer) {
+      setScore(score + 1);
+    }
+
+    setAttempts(attempts + 1);
+    if (attempts === 20) {
+      alert(`After 20 problem your score is ${score}`);
+      setShow(1);
+    }
+
+    // Генерация нового примера после проверки
+    console.log(attempts);
+    generateExample();
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      checkAnswer();
+    }
+  };
+
+  useEffect(() => {
+    generateExample();
+  }, []);
+
+  return (
+    <Layout setShow={setShow}>
+      <div className="flex justify-center items-center flex-col">
+        <div className="flex flex-col items-center max-md:flex-col">
+          <div className="text-center text-5xl">Score: {score}</div>
+          <div className="text-center text-5xl">Attempts: {attempts}</div>
+          <div className="text-center text-8xl">
+            {num1} {operation} {num2}
+          </div>
+        </div>
+        <div className="w-full max-w-sm min-w-[200px]">
+          <input
+            className="w-full bg-transparent placeholder:text-green-400 text-green-700 text-sm border border-green-400 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-green-500 hover:border-green-600 shadow-sm focus:shadow"
+            placeholder="Type here..."
+            type="number"
+            value={userAnswer}
+            onChange={(e) => setUserAnswer(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <button
+          className="bg-green-400 hover:bg-green-600 duration-[400ms] text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-3"
+          onClick={checkAnswer}
+        >
+          Enter
+        </button>
       </div>
-    </div>
+    </Layout>
   );
 };
 
--- src/components/Timeless/Timeless.jsx
+++ src/components/Timeless/Timeless.jsx
@@ -1,27 +1,104 @@
-import TimelessEnd from "./TimelessEnd";
+import { useEffect, useState } from "react";
+import Layout from "../../Layout";
 
 // eslint-disable-next-line react/prop-types
 const Timeless = ({ setShow }) => {
-  return (
-    <div className="bg-[#faebd9] flex flex-col items-center mt-[7%] mx-[20%] max-md:mx-[8%] rounded-xl shadow-xl h-[70vh]">
-      <div className="flex flex-row w-full justify-between">
-        <div className="flex flex-row gap-1 mt-2 ml-2">
-          <div className="h-4 w-4 border-fancy-1 bg-[#fea699] border-2 border-black" />
-          <div className="h-4 w-4 border-fancy-2 bg-[#ffae9d] border-2 border-black" />
-          <div className="h-4 w-4 border-fancy-3 bg-[#807b82] border-2 border-black" />
-        </div>
-      </div>
-      <div className="">
-        <div
-          className="text-6xl text-center text-slate-700 hover:cursor-pointer"
-          onClick={() => setShow(1)}
-        >
-          NUMFIGHT
-        </div>
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [operation, setOperation] = useState("+");
+  const [userAnswer, setUserAnswer] = useState("");
+  const [score, setScore] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(60);
 
-        <TimelessEnd />
+  const generateRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const generateRandomOperation = () => {
+    const operations = ["+", "-", "*"];
+    const randomIndex = Math.floor(Math.random() * operations.length);
+    return operations[randomIndex];
+  };
+
+  const generateExample = () => {
+    const operation = generateRandomOperation();
+    const num1 = generateRandomNumber(1, operation === "*" ? 10 : 100);
+    const num2 = generateRandomNumber(1, operation === "*" ? 10 : 100);
+
+    setNum1(num1);
+    setNum2(num2);
+    setOperation(operation);
+    setUserAnswer("");
+  };
+
+  const checkAnswer = () => {
+    let correctAnswer = eval(`${num1} ${operation} ${num2}`);
+    console.log(correctAnswer);
+    if (parseInt(userAnswer) === correctAnswer) {
+      setScore(score + 1);
+    }
+
+    // Генерация нового примера после проверки
+    generateExample();
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      checkAnswer();
+    }
+  };
+
+  useEffect(() => {
+    generateExample();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((time) => {
+        if (time > 0) {
+          return time - 1;
+        } else {
+          clearInterval(time);
+          alert(`The game is over. Your score is ${score}`);
+          setShow(1);
+          return 0;
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Layout setShow={setShow}>
+      <div className="flex justify-center items-center flex-col">
+        <div className="flex flex-col items-center max-md:flex-col gap-1 max-md:gap-0">
+          <div className="text-center text-5xl">Score: {score}</div>
+          <div className="text-center text-5xl">Time left: {timeLeft}</div>
+          <div className="text-center text-8xl">
+            {num1} {operation} {num2}
+          </div>
+        </div>
+        <div className="w-full max-w-sm min-w-[200px] mt-2">
+          <input
+            className="w-full bg-transparent placeholder:text-green-400 text-green-700 text-sm border border-green-400 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-green-500 hover:border-green-600 shadow-sm focus:shadow"
+            placeholder="Type here..."
+            type="number"
+            value={userAnswer}
+            onChange={(e) => setUserAnswer(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+        </div>
+        <button
+          className="bg-green-400 hover:bg-green-600 duration-[400ms] text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center mt-3"
+          onClick={checkAnswer}
+        >
+          Enter
+        </button>
       </div>
-    </div>
+    </Layout>
   );
 };
 
