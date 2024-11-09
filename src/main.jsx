e220d59dac8f7424d22a658910fa70028dd3ba60
Platon <platon20081019@gmail.com>
2024-11-10 00:55:05 +0200

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("Service Worker registered"))
+      .catch((err) => console.log("Service Worker registration failed", err));
+  });
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
